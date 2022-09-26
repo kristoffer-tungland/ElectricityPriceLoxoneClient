560b8fed673cfb2772b4882913ae363ecfb1157d
Kristoffer Tungland <ktu@cowi.com>
2022-09-26 09:48:07 +0200

    Smal fixes

--- HttpGetProgram/HttpGetProgram.c
+++ HttpGetProgram/HttpGetProgram.c
@@ -1,7 +1,7 @@
 #include <wtypes.h>
 #include <stdio.h>
 #include <string.h>
-#include <Windows.h>
+#include <windows.h>
 #include <time.h>
 
 char* strfind(const char* haystack, const char* needle, int offset)
@@ -16,12 +16,18 @@ char* strfind(const char* haystack, const char* needle, int offset)
 
 char* httpget(char* adress, char* page)
 {
-	return "{\r\n  \"ScoreNow\": 22,\r\n  \"HourNow\": 8,\r\n  \"PriceNow\": 4.1345735,\r\n  \"PriceUnit\": \"NOK\/kWh\",\r\n  \"HourScores\": {\r\n    \"ScoreOfHour0\": 1,\r\n    \"ScoreOfHour1\": 4,\r\n    \"ScoreOfHour2\": 3,\r\n    \"ScoreOfHour3\": 2,\r\n    \"ScoreOfHour4\": 5,\r\n    \"ScoreOfHour5\": 6,\r\n    \"ScoreOfHour6\": 15,\r\n    \"ScoreOfHour7\": 21,\r\n    \"ScoreOfHour8\": 22,\r\n    \"ScoreOfHour9\": 18,\r\n    \"ScoreOfHour10\": 14,\r\n    \"ScoreOfHour11\": 11,\r\n    \"ScoreOfHour12\": 8,\r\n    \"ScoreOfHour13\": 9,\r\n    \"ScoreOfHour14\": 10,\r\n    \"ScoreOfHour15\": 7,\r\n    \"ScoreOfHour16\": 12,\r\n    \"ScoreOfHour17\": 17,\r\n    \"ScoreOfHour18\": 20,\r\n    \"ScoreOfHour19\": 24,\r\n    \"ScoreOfHour20\": 23,\r\n    \"ScoreOfHour21\": 19,\r\n    \"ScoreOfHour22\": 16,\r\n    \"ScoreOfHour23\": 13\r\n  },\r\n  \"HourPrices\": {\r\n    \"PriceOfHour0\": 0.88227224,\r\n    \"PriceOfHour1\": 1.0211757,\r\n    \"PriceOfHour2\": 0.9696734,\r\n    \"PriceOfHour3\": 0.91398954,\r\n    \"PriceOfHour4\": 1.0349437,\r\n    \"PriceOfHour5\": 1.8353221,\r\n    \"PriceOfHour6\": 3.5773277,\r\n    \"PriceOfHour7\": 4.0668554,\r\n    \"PriceOfHour8\": 4.1345735,\r\n    \"PriceOfHour9\": 3.7363222,\r\n    \"PriceOfHour10\": 3.4642262,\r\n    \"PriceOfHour11\": 2.4794593,\r\n    \"PriceOfHour12\": 2.2932346,\r\n    \"PriceOfHour13\": 2.4155147,\r\n    \"PriceOfHour14\": 2.4211237,\r\n    \"PriceOfHour15\": 2.2671266,\r\n    \"PriceOfHour16\": 2.623258,\r\n    \"PriceOfHour17\": 3.6836982,\r\n    \"PriceOfHour18\": 4.0540056,\r\n    \"PriceOfHour19\": 4.3685274,\r\n    \"PriceOfHour20\": 4.179141,\r\n    \"PriceOfHour21\": 3.889912,\r\n    \"PriceOfHour22\": 3.6065977,\r\n    \"PriceOfHour23\": 3.1162534\r\n  }\r\n}";
+	char* price = "{\r\n  \"ScoreNow\": 22,\r\n  \"HourNow\": 8,\r\n  \"PriceNow\": 4.1345735,\r\n  \"PriceUnit\": \"NOK\/kWh\",\r\n  \"HourScores\": {\r\n    \"ScoreOfHour0\": 1,\r\n    \"ScoreOfHour1\": 4,\r\n    \"ScoreOfHour2\": 3,\r\n    \"ScoreOfHour3\": 2,\r\n    \"ScoreOfHour4\": 5,\r\n    \"ScoreOfHour5\": 6,\r\n    \"ScoreOfHour6\": 15,\r\n    \"ScoreOfHour7\": 21,\r\n    \"ScoreOfHour8\": 22,\r\n    \"ScoreOfHour9\": 18,\r\n    \"ScoreOfHour10\": 14,\r\n    \"ScoreOfHour11\": 11,\r\n    \"ScoreOfHour12\": 8,\r\n    \"ScoreOfHour13\": 9,\r\n    \"ScoreOfHour14\": 10,\r\n    \"ScoreOfHour15\": 7,\r\n    \"ScoreOfHour16\": 12,\r\n    \"ScoreOfHour17\": 17,\r\n    \"ScoreOfHour18\": 20,\r\n    \"ScoreOfHour19\": 24,\r\n    \"ScoreOfHour20\": 23,\r\n    \"ScoreOfHour21\": 19,\r\n    \"ScoreOfHour22\": 16,\r\n    \"ScoreOfHour23\": 13\r\n  },\r\n  \"HourPrices\": {\r\n    \"PriceOfHour0\": 0.88227224,\r\n    \"PriceOfHour1\": 1.0211757,\r\n    \"PriceOfHour2\": 0.9696734,\r\n    \"PriceOfHour3\": 0.91398954,\r\n    \"PriceOfHour4\": 1.0349437,\r\n    \"PriceOfHour5\": 1.8353221,\r\n    \"PriceOfHour6\": 3.5773277,\r\n    \"PriceOfHour7\": 4.0668554,\r\n    \"PriceOfHour8\": 4.1345735,\r\n    \"PriceOfHour9\": 3.7363222,\r\n    \"PriceOfHour10\": 3.4642262,\r\n    \"PriceOfHour11\": 2.4794593,\r\n    \"PriceOfHour12\": 2.2932346,\r\n    \"PriceOfHour13\": 2.4155147,\r\n    \"PriceOfHour14\": 2.4211237,\r\n    \"PriceOfHour15\": 2.2671266,\r\n    \"PriceOfHour16\": 2.623258,\r\n    \"PriceOfHour17\": 3.6836982,\r\n    \"PriceOfHour18\": 4.0540056,\r\n    \"PriceOfHour19\": 4.3685274,\r\n    \"PriceOfHour20\": 4.179141,\r\n    \"PriceOfHour21\": 3.889912,\r\n    \"PriceOfHour22\": 3.6065977,\r\n    \"PriceOfHour23\": 3.1162534\r\n  }\r\n}";
+	char* result;
+	int lenght = strlen(price);
+	result = (char*)malloc(sizeof(char) * lenght);
+	strncpy(result, price, lenght);
+
+	return result;
 }
 
 void setoutputtext(int port, char* value)
 {
-	printf("Text output: %d\r\n%s\r\n", port, value);
+	//printf("\r\n\r\nText output: %d\r\n%s\r\n", port, value);
 }
 
 int getinput(int port)
@@ -42,32 +48,32 @@ int getinput(int port)
 void setHourScores(char* result, int hourScorePos, int hourPricesPos)
 {
 	int lenght = hourPricesPos - hourScorePos;
-	char* hourScoreStemp = calloc(1, lenght * sizeof(char));
+	char* hourScore = calloc(1, lenght * sizeof(char));
 
-	strncpy(hourScoreStemp, result + hourScorePos, lenght);
+	strncpy(hourScore, result + hourScorePos, lenght);
 
-	printf("stemp: %s", hourScoreStemp);
+	printf("hourScore:\r\n%s\r\n", hourScore);
 
-	setoutputtext(0, hourScoreStemp);
+	setoutputtext(0, hourScore);
 
-	free(hourScoreStemp);
-	hourScoreStemp = 0;
+	free(hourScore);
+	hourScore = 0;
 }
 
 void setHourPrices(char* result, int hourPricesPos)
 {
 	int resultLenght = strlen(result);
 	int lenght = resultLenght - hourPricesPos;
-	char* hourPriceStemp = calloc(1, resultLenght * sizeof(char));
+	char* hourPrice = calloc(1, lenght * sizeof(char));
 
-	strncpy(hourPriceStemp, result + hourPricesPos, lenght);
+	strncpy(hourPrice, result + hourPricesPos, lenght);
 
-	printf("stemp: %s", hourPriceStemp);
+	printf("hourPrice:\r\n%s\r\n", hourPrice);
 
-	setoutputtext(1, hourPriceStemp);
+	setoutputtext(1, hourPrice);
 
-	free(hourPriceStemp);
-	hourPriceStemp = 0;
+	free(hourPrice);
+	hourPrice = 0;
 }
 
 
@@ -94,8 +100,7 @@ void getPrice(char* endpoint)
 		splitOutputs(result);
 	}
 
-	// Un-comment free()
-	//free(result);
+	free(result);
 }
 
 void getTodaysPrice()
@@ -145,6 +150,8 @@ int main()
 
 		// Slow the loop down 10 minutes
 		int sleepTime = 10 * 60 * 1000;
-		//sleep(sleepTime);
+		Sleep(sleepTime);
 	}
+
+	return 0;
 }
